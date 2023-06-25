7e2196b40b8bc6ed2169b357fa592af7b1f038e9
rutyak <132219103+rutyak@users.noreply.github.com>
2023-06-26 01:06:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+ window.addEventListener('resize', updateSize);
+
+    function updateSize() {
+      var width = window.innerWidth;
+      var height = window.innerHeight;
+
+      var dimensionsElement = document.getElementById('dimensions');
+      dimensionsElement.textContent = 'Width: ' + width + ', Height: ' + height;
+    }
